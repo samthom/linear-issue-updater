493f259b21ae119b4e58f01111d9c2160f1a9b9e
Sam Thomas <samthomas@Sams-MacBook-Air.local>
2026-04-16 14:52:36 -0600

    feat: Pull request linking in linear
    Api end point for pr linking to the linear issue. Add dotenv support
    for api key and add makefile for easy command reference

--- main.py
+++ main.py
@@ -1,6 +1,63 @@
-def main():
-    print("Hello from linear-issue-updater!")
+from flask import Flask, request
+from dotenv import load_dotenv
+import requests
+import re
+import os
 
+load_dotenv()
+
+app = Flask(__name__)
+
+@app.route('/')
+def health_check():
+    return "."
+
+@app.route('/sync-linear', methods=['POST'])
+def sync_linear():
+    event = request.headers.get("x-event-key")
+    payload = request.get_json()
+    pr_data = payload.get("pullrequest", {})
+    pr_title = pr_data.get("title", "")
+    pr_url = pr_data.get("links", {}).get("html", {}).get("href")
+    
+    match = re.search(r'[A-Z]+-\d+', pr_title)
+    if not match:
+        return { "message": "No linear issue ID found"}, 202
+
+    linear_issue_id = match.group(0)
+    api_key = os.environ.get('LINEAR_API_KEY', '')
+
+    mutation = """
+    mutation CreateAttachment($issueId: String!, $url: String!, $title: String!) {
+          attachmentCreate(input: {
+            issueId: $issueId,
+            url: $url,
+            title: $title,
+            iconUrl: "https://bitbucket.org/favicon.ico"
+          }) {
+            success
+          }
+        }
+    """
+    vars = {
+        "issueId": linear_issue_id,
+        "url": pr_url,
+        "title": f"Bitbucket PR: {pr_title}"
+    }
+
+    r = requests.post(
+        "https://api.linear.app/graphql",
+        headers={"Authorization": api_key},
+        json={"query": mutation, "variables": vars}
+    )
+    if r.status_code != 200:
+        response = r.json()
+        errors = response.get('errors')
+        error = errors[0]
+        content = { "message": error.get('message', '') }
+        return content, r.status_code
+    
+    return { "message": "Success" }, 200
 
 if __name__ == "__main__":
-    main()
+    app.run(debug=True, port=8000)
