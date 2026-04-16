57a4f350e8c446c8cff40cee5249dd0e86afe3bb
Sam Thomas <samthomas@Sams-MacBook-Air.local>
2026-04-16 16:04:03 -0600

    test(pr-done): Test for issue status update to done
    Write test for the issue update feature to done. Mask the real api_key
    and add mock client assertions with payload

--- test_app.py
+++ test_app.py
@@ -1,6 +1,10 @@
 import pytest
 from unittest.mock import patch
 from main import app
+import os
+
+api_key = "very_secret"
+os.environ["LINEAR_API_KEY"] = api_key
 
 @pytest.fixture
 def client():
@@ -10,13 +14,16 @@ def client():
 
 @patch('main.requests.post')
 def test_sync_linear_pr_linking_success(mock_post, client):
+    linear_issue_id = "RND-118"
+    pr_title = f"{linear_issue_id} Title of the pull request"
+    pr_url = "https://api.bitbucket.org/pullrequest_id"
     payload = {
         "pullrequest": {
-            "title": "RND-118 Title of pull request",
+            "title": pr_title,
             "description": "Description of pull request",
             "links": {
                 "html": {
-                    "href": "https://api.bitbucket.org/pullrequest_id"
+                    "href": pr_url
                 }
             }
         }
@@ -28,9 +35,32 @@ def test_sync_linear_pr_linking_success(mock_post, client):
     mock_post.return_value.status_code = 200
     response = client.post('/sync-linear', json=payload, headers=headers)
     assert response.status_code == 200
+
+    mutation = """
+        mutation CreateAttachment($issueId: String!, $url: String!, $title: String!) {
+          attachmentCreate(input: {
+            issueId: $issueId,
+            url: $url,
+            title: $title,
+            iconUrl: "https://bitbucket.org/favicon.ico"
+          }) {
+            success
+          }
+        }
+        """
+    vars = {
+        "issueId": linear_issue_id,
+        "url": pr_url,
+        "title": f"Bitbucket PR: {pr_title}"
+    }
     
-@patch('main.requests.post')
-def test_sync_linear_pr_linking_no_issue(mock_post, client):
+    mock_post.assert_called_once_with(
+        "https://api.linear.app/graphql",
+        headers={"Authorization": api_key},
+        json={"query": mutation, "variables": vars}
+    )
+    
+def test_sync_linear_pr_linking_no_issue(client):
     payload = {
         "pullrequest": {
             "title": "Title of pull request",
@@ -46,8 +76,46 @@ def test_sync_linear_pr_linking_no_issue(mock_post, client):
     headers = {
         "x-event-key": "pullrequest:created",
     }
-    mock_post.return_value.status_code = 200
     response = client.post('/sync-linear', json=payload, headers=headers)
     assert response.status_code == 202
     
     
+
+@patch('main.requests.post')
+def test_sync_linear_pr_done_success(mock_post, client):
+    linear_issue_id = "RND-118"
+    pr_title = f"{linear_issue_id} Title of the pull request"
+    pr_url = "https://api.bitbucket.org/pullrequest_id"
+    payload = {
+        "pullrequest": {
+            "title": pr_title,
+            "description": "Description of pull request",
+            "links": {
+                "html": {
+                    "href": pr_url
+                }
+            }
+        }
+    }
+    
+    headers = {
+        "x-event-key": "pullrequest:fulfilled",
+    }
+    mock_post.return_value.status_code = 200
+    response = client.post('/sync-linear', json=payload, headers=headers)
+    assert response.status_code == 200
+
+    mutation = """
+        mutation UpdateStatus($id: String!) {
+          issueUpdate(id: $id, input: { stateId: "46746634-8c8e-4707-9179-1a663eb9ccbf" }) {
+            success
+          }
+        }
+        """
+    vars = {"id": linear_issue_id}
+    
+    mock_post.assert_called_once_with(
+        "https://api.linear.app/graphql",
+        headers={"Authorization": api_key},
+        json={"query": mutation, "variables": vars}
+    )
